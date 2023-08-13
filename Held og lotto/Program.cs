e598bffbb44941fe8786b2ff9d8e38c9dc2db03e
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2023-08-13 23:07:57 +0200

    Updated assignment

--- Held og lotto/Program.cs
+++ Held og lotto/Program.cs
@@ -10,31 +10,52 @@ namespace Held_og_lotto
     {
         static void Main(string[] args)
         {
+            // Loops until another key than R is pressed when asked
             do
             {
-                // Instance of the Random class
-                Random rand = new Random();
+                // Waits for an user to press any key
+                Console.WriteLine("Press any key to draw a new lottery ticket.");
+                Console.ReadKey(true);
 
-                // Gets 7 random numbers
-                int[] test = new int[8];
-                for (int i = 0; i < 8; i++)
-                    test[i] = rand.Next(1, 37);
+                Console.Clear();
+                Console.WriteLine("Your lottery ticket:");
 
-                // Sort the 7 random numbers without a joker
-                int joker = test[test.Length - 1];
-                Array.Sort(test);
+                // Draws a new lottery ticket
+                DrawTicket();
 
-                // Writes the 7 random numbers with a delay of 2 seconds (2000ms)
-                for (int i = 0; i < test.Length - 1; i++)
-                {
-                    Console.Write(test[i] + "   ");
-                    System.Threading.Thread.Sleep(2000);
-                }
-
-                // Writes the joker number
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.Write(joker + "\nPress R to draw new lottery ticket, or any other key for closing the program");
+                // Gives the user a opportunity to draw a new lottery ticket
+                Console.ResetColor();
+                Console.Write("\nPress R to draw a new lottery ticket, or any other key for closing the program");
             } while (Console.ReadKey(true).Key == ConsoleKey.R);
+        }
+
+        /// <summary>
+        /// Draws a new ticket
+        /// </summary>
+        static void DrawTicket()
+        {
+            // Instance of the Random class
+            Random rand = new Random();
+
+            // Gets 7 random numbers
+            int[] test = new int[8];
+            for (int i = 0; i < 8; i++)
+                test[i] = rand.Next(1, 37);
+
+            // Sort the 7 random numbers without a joker
+            int joker = test[test.Length - 1];
+            Array.Sort(test);
+
+            // Writes the 7 random numbers with a delay of 2 seconds (2000ms)
+            for (int i = 0; i < test.Length - 1; i++)
+            {
+                Console.Write(test[i] + "   ");
+                System.Threading.Thread.Sleep(2000);
+            }
+
+            // Writes the joker number
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(joker);
         }
     }
 }
