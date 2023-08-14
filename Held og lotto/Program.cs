ef973672b7618ac6629ac5e5a62324baf3f4a488
Nicklas Lindegaard Martlev Gustavsen <nicklasgustavsen75@gmail.com>
2023-08-14 13:24:50 +0200

    Updated assignment

--- Held og lotto/Program.cs
+++ Held og lotto/Program.cs
@@ -10,12 +10,13 @@ namespace Held_og_lotto
     {
         static void Main(string[] args)
         {
+            // Waits for an user to press any key
+            Console.WriteLine("Press any key to draw a new lottery ticket.");
+            Console.ReadKey(true);
+
             // Loops until another key than R is pressed when asked
             do
             {
-                // Waits for an user to press any key
-                Console.WriteLine("Press any key to draw a new lottery ticket.");
-                Console.ReadKey(true);
 
                 Console.Clear();
                 Console.WriteLine("Your lottery ticket:");
@@ -25,8 +26,8 @@ namespace Held_og_lotto
 
                 // Gives the user a opportunity to draw a new lottery ticket
                 Console.ResetColor();
-                Console.Write("\nPress R to draw a new lottery ticket, or any other key for closing the program");
-            } while (Console.ReadKey(true).Key == ConsoleKey.R);
+                Console.Write("\nPress Enter to draw a new lottery ticket, or any other key for closing the program");
+            } while (Console.ReadKey(true).Key == ConsoleKey.Enter);
         }
 
         /// <summary>
@@ -37,19 +38,32 @@ namespace Held_og_lotto
             // Instance of the Random class
             Random rand = new Random();
 
-            // Gets 7 random numbers
-            int[] test = new int[8];
-            for (int i = 0; i < 8; i++)
-                test[i] = rand.Next(1, 37);
+            // Array with length of 8 which the lottery numbers will be put in
+            int[] ticketNumbers = new int[8];
+
+            // Counter for while loop
+            int counter = 0;
+
+            // Sets the index of the array to the random number if
+            // the array doesn't contain the number already
+            while (ticketNumbers[7] == 0)
+            {
+                int randomNum = rand.Next(1, 37);
+                if(!ticketNumbers.Contains(randomNum))
+                {
+                    ticketNumbers[counter] = randomNum; 
+                }
+                counter++;
+            }
 
             // Sort the 7 random numbers without a joker
-            int joker = test[test.Length - 1];
-            Array.Sort(test);
+            int joker = ticketNumbers[ticketNumbers.Length - 1];
+            Array.Sort(ticketNumbers);
 
             // Writes the 7 random numbers with a delay of 2 seconds (2000ms)
-            for (int i = 0; i < test.Length - 1; i++)
+            for (int i = 0; i < ticketNumbers.Length - 1; i++)
             {
-                Console.Write(test[i] + "   ");
+                Console.Write(ticketNumbers[i] + "   ");
                 System.Threading.Thread.Sleep(2000);
             }
 
